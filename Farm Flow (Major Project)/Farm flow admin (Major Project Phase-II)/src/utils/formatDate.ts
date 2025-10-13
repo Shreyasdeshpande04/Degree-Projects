63f5d7c21f82b70edfe7a09ef90c246ee86a64e3
Shreyas Deshpande <deshpandeshreyas434@gmail.com>
2025-10-13 19:22:20 +0530

    Deleted Farm Flow (Major Project) folder

--- Farm Flow (Major Project)/Farm flow admin (Major Project Phase-II)/src/utils/formatDate.ts
+++
@@ -1,8 +0,0 @@
-export function extractDateAndTime(placedAt?: string) {
-  if (!placedAt) { return { date: '', time: '' }; }
-  const [datePart, rest] = placedAt.split(' at ');
-  if (!rest) { return { date: placedAt, time: '' }; }
-  const timeMatch = rest.match(/(\d{1,2}:\d{2})[^\d]*(AM|PM)/i);
-  const time = timeMatch ? `${timeMatch[1]} ${timeMatch[2]}` : rest;
-  return { date: datePart, time };
-}
